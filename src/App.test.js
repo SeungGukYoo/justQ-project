b8d57b4efddf9ea87277780a5c859edc82ca0a17
SeungGukYoo <dbtmd878@gmail.com>
2023-09-25 15:29:06 +0900

    test: 홈 화면 UI 테스트 코드 추가

---
+++ src/App.test.js
@@ -0,0 +1,37 @@
+import "@testing-library/jest-dom";
+import { render, screen, within } from "@testing-library/react";
+import App from "./App";
+
+test("only renders a UI", () => {
+  render(<App />);
+
+  const header = screen.getByText("Just Q Shopping List");
+  const details = screen.getByText("페이지 별 상품 수");
+  const perPage = screen.getByTestId("per-page");
+  const pageCount = screen.getByTestId("page-count");
+  const perPageList = within(perPage).getAllByRole("listitem");
+
+  const pageCountList = within(pageCount).getAllByRole("listitem");
+  const nextPage = screen.getByRole("button", {
+    name: /다음 페이지/,
+  });
+  const prePage = screen.getByRole("button", {
+    name: /이전 페이지/,
+  });
+
+  const firstPage = screen.getByRole("button", {
+    name: /첫 페이지/,
+  });
+  const lastPage = screen.getByRole("button", {
+    name: /마지막 페이지/,
+  });
+
+  expect(perPageList).toHaveLength(5);
+  expect(pageCountList).toHaveLength(5);
+  expect(prePage).toBeInTheDocument();
+  expect(nextPage).toBeInTheDocument();
+  expect(firstPage).toBeInTheDocument();
+  expect(lastPage).toBeInTheDocument();
+  expect(header).toBeInTheDocument();
+  expect(details).toBeInTheDocument();
+});
