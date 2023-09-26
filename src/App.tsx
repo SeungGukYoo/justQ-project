ce3ea281485ba8907e18e7133e976bbb3a502581
SeungGukYoo <dbtmd878@gmail.com>
2023-09-26 15:23:42 +0900

    feat: 페이지당 보여지는 갯수 수정시 details 접히는 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 import List from "./components/list";
 import { HttpClient } from "./util/httpClient";
@@ -26,7 +26,7 @@ function App({ httpClient }: Prop) {
   const [perPageCount, setPerPageCount] = useState(30);
   const [totalPageCount, setTotalPageCount] = useState(0);
   const [currentPage, setCurrentPageCount] = useState(0);
-
+  const detailsRef = useRef<null | HTMLDetailsElement>(null);
   useEffect(() => {
     const callData = async () => {
       try {
@@ -55,50 +55,59 @@ function App({ httpClient }: Prop) {
     if (parseInt(e.currentTarget.innerText) === perPageCount) return;
     setPerPageCount(parseInt(e.currentTarget.innerText));
     setCurrentPageCount(0);
+    if (detailsRef.current) {
+      detailsRef.current.open = false;
+    }
   };
   return (
     <div className="home">
       <h1 className="title">Just Q Shopping List</h1>
-      <details className="perPage">
-        <summary>페이지 별 상품 수</summary>
-        <ul data-testid="per-page">
-          {itemPerCount.map((itemCount, idx) => (
-            <li
-              key={idx}
-              style={{
-                background: `${itemCount === perPageCount ? "rgba(0, 0, 0, 0.6)" : "rgba(0, 0, 0, 0.2)"}`,
-              }}
-              onClick={changePerPageCount}
-            >
-              {itemCount}
-            </li>
-          ))}
-        </ul>
-      </details>
-      <div className="itemList">
-        {itemList.length ? <List itemList={itemList} /> : <p className="loadingText">Loading...</p>}
-      </div>
-      <div className="navigationBar">
-        <button onClick={() => pageMove(0)}>&lt;&lt;</button>
-        <button onClick={() => pageMove(currentPage - 1)}>&lt;</button>
-        <ul data-testid="page-count">
-          {Array.from({ length: totalPageCount }, (_, idx) => {
-            return (
-              <li
-                onClick={() => pageMove(idx)}
-                style={{
-                  color: `${idx + 1 === currentPage + 1 ? "black" : "rgba(128, 128, 128, 0.7)"}`,
-                }}
-                key={idx}
-              >
-                {idx + 1}
-              </li>
-            );
-          })}
-        </ul>
-        <button onClick={() => pageMove(currentPage + 1)}>&gt;</button>
-        <button onClick={() => pageMove(totalPageCount - 1)}>&gt;&gt;</button>
-      </div>
+      {itemList.length ? (
+        <>
+          <details className="perPage" ref={detailsRef}>
+            <summary>페이지 별 상품 수</summary>
+            <ul data-testid="per-page">
+              {itemPerCount.map((itemCount, idx) => (
+                <li
+                  key={idx}
+                  style={{
+                    background: `${itemCount === perPageCount ? "rgba(0, 0, 0, 0.6)" : "rgba(0, 0, 0, 0.2)"}`,
+                  }}
+                  onClick={changePerPageCount}
+                >
+                  {itemCount}
+                </li>
+              ))}
+            </ul>
+          </details>
+          <div className="itemList">
+            <List itemList={itemList} />
+          </div>
+          <div className="navigationBar">
+            <button onClick={() => pageMove(0)}>&lt;&lt;</button>
+            <button onClick={() => pageMove(currentPage - 1)}>&lt;</button>
+            <ul data-testid="page-count">
+              {Array.from({ length: totalPageCount }, (_, idx) => {
+                return (
+                  <li
+                    onClick={() => pageMove(idx)}
+                    style={{
+                      color: `${idx + 1 === currentPage + 1 ? "black" : "rgba(128, 128, 128, 0.7)"}`,
+                    }}
+                    key={idx}
+                  >
+                    {idx + 1}
+                  </li>
+                );
+              })}
+            </ul>
+            <button onClick={() => pageMove(currentPage + 1)}>&gt;</button>
+            <button onClick={() => pageMove(totalPageCount - 1)}>&gt;&gt;</button>
+          </div>
+        </>
+      ) : (
+        <p className="loadingText">Loading...</p>
+      )}
     </div>
   );
 }
