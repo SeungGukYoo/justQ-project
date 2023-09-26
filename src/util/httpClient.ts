767d9662929ab7bed42be45459105722474b65cf
SeungGukYoo <dbtmd878@gmail.com>
2023-09-26 23:46:19 +0900

    feat: 타입 추거

--- src/util/httpClient.ts
+++ src/util/httpClient.ts
@@ -1,12 +1,18 @@
+import { ResponseData } from "../..";
+
 interface IHttpClient {
-  baseUrl: string;
-  get(): Promise<Response>;
+  get(): Promise<Array<ResponseData>>;
 }
 
 export class HttpClient implements IHttpClient {
-  baseUrl = "https://just-q-json-server-vercel.vercel.app/posts";
-  async get() {
-    const data = await fetch(this.baseUrl);
+  #baseUrl: string;
+
+  constructor() {
+    this.#baseUrl = "https://just-q-json-server-vercel.vercel.app/posts";
+  }
+
+  async get(): Promise<Array<ResponseData>> {
+    const data = await fetch(this.#baseUrl);
     return data.json();
   }
 }
