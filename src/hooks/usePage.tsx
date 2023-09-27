d97b38692c501b24ce8c4d8f476e2e8d69f8b25c
SeungGukYoo <dbtmd878@gmail.com>
2023-09-27 14:17:40 +0900

    feat: 세션을 외부 함수를 통해 받아와 state의 초기값으로 사용

--- src/hooks/usePage.tsx
+++ src/hooks/usePage.tsx
@@ -2,14 +2,15 @@ import { useCallback, useState } from "react";
 
 import { ResponseData } from "../..";
 import { HttpClient } from "../util/httpClient";
+import { isSavedSessionData } from "../util/sessionClient";
 
 const httpClient = new HttpClient();
 
 export default function usePage() {
   const [itemList, setItemList] = useState<[] | ResponseData[]>([]);
-  const [perPageCount, setPerPageCount] = useState(30);
+  const [perPageCount, setPerPageCount] = useState(isSavedSessionData().perPageCount || 30);
   const [totalPageCount, setTotalPageCount] = useState(0);
-  const [currentPage, setCurrentPageCount] = useState(0);
+  const [currentPage, setCurrentPageCount] = useState(isSavedSessionData().pageCount || 0);
 
   const updatePage = (pageNumber: number) => {
     setCurrentPageCount(pageNumber);
@@ -20,26 +21,23 @@ export default function usePage() {
     setCurrentPageCount(0);
   };
 
-  const updateList = useCallback(
-    async (perPage: number = perPageCount, page: number = currentPage) => {
-      try {
-        const data = await httpClient.get();
-        const convertDataToArray: Array<ResponseData[]> = [];
-        for (let i = 0; i < Math.ceil(data.length / perPage); i++) {
-          convertDataToArray.push(data.slice(i * perPage, (i + 1) * perPage));
-        }
-        setTotalPageCount(Math.ceil(data.length / perPage));
-        setItemList(convertDataToArray[page]);
-        setPerPageCount(perPage);
-        setCurrentPageCount(page);
-      } catch (error) {
-        if (error instanceof Error) {
-          throw new Error(error.message);
-        }
+  const updateList = useCallback(async () => {
+    try {
+      const data = await httpClient.get();
+      const convertDataToArray: Array<ResponseData[]> = [];
+      for (let i = 0; i < Math.ceil(data.length / perPageCount); i++) {
+        convertDataToArray.push(data.slice(i * perPageCount, (i + 1) * perPageCount));
       }
-    },
-    [currentPage, perPageCount]
-  );
+      setTotalPageCount(Math.ceil(data.length / perPageCount));
+      setItemList(convertDataToArray[currentPage]);
+      setPerPageCount(perPageCount);
+      setCurrentPageCount(currentPage);
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new Error(error.message);
+      }
+    }
+  }, [currentPage, perPageCount]);
 
   return { perPageCount, currentPage, updatePage, updatePerPage, itemList, updateList, totalPageCount };
 }
